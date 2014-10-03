8fc40ddd79bbce182d3fd3db063a960609194aa9
Gabriel Codina <gabeeecodina@gmail.com>
2014-10-03 14:55:44 -0700

    first snake commit

---
+++ public_html/js/snake.js
@@ -0,0 +1,57 @@
+var snake;
+var snakeLength;
+var snakeSize;
+
+
+
+var context;
+var screenWidth;
+var screenHeight;
+
+gameInitialize();
+snakeInitialize();
+gameDraw();
+snakeDraw();
+
+function gameInitialize(){
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+   
+}
+function gameLoop() {
+    
+}
+function gameDraw() {
+    context.fillStyle = "rgb(26,46,145)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitialize(){
+    snake = [];
+    snakeLength = 4;
+    snakeSize = 15;
+    
+    for (var index = snakeLength - 1; index >= 0; index--){
+    snake.push({
+        x: index,
+        y: 0
+    });
+}
+    
+}
+
+
+
+function snakeDraw(){
+    for (var index = 0; index < snake.length; index++){
+        context.fillStyle = "black";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }
+}
+function snakeUpdate(){
+    
+}
